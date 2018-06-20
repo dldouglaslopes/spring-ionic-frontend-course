85b789d1a3ab81b813b8b07f60073b121b8d0c62
dldouglaslopes <dldouglaslopes@gmail.com>
2018-06-20 17:00:31 -0300

    Show categories

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoryService } from '../../services/domain/category.services';
+import { CategoryDTO } from '../../models/Category.dto';
 
 /**
  * Generated class for the CategoriesPage page.
@@ -16,6 +17,8 @@ import { CategoryService } from '../../services/domain/category.services';
 })
 export class CategoriesPage {
 
+  items: CategoryDTO[]
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -25,7 +28,7 @@ export class CategoriesPage {
   ionViewDidLoad() {
     this.categoryService.findAll()
         .subscribe(response => {
-          console.log(response);          
+          this.items = response;   
         },
         error => {
           console.log(error);
