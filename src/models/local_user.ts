59e6e508d690c24453a9cb69ec290d8debfed27b
dldouglaslopes <dldouglaslopes@gmail.com>
2018-06-25 19:34:35 -0300

    Create a profile page

--- src/models/local_user.ts
+++ src/models/local_user.ts
@@ -1,7 +1,4 @@
 export interface LocalUser{
     token: string;
-<<<<<<< HEAD
     email: string;
-=======
->>>>>>> 20ae5ed85df034746858a297f89e660e4948308c
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,7 +4,6 @@ import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../config/api.config";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
-<<<<<<< HEAD
 import { JwtHelper } from "angular2-jwt";
  
 @Injectable()
@@ -12,12 +11,6 @@ export class AuthService {
 
     jwtHelper: JwtHelper = new JwtHelper();
 
-=======
-
-@Injectable()
-export class AuthService {
-
->>>>>>> 20ae5ed85df034746858a297f89e660e4948308c
     constructor(public http: HttpClient, public storage: StorageService) {
     }
 
@@ -34,12 +27,8 @@ export class AuthService {
     successfulLogin(authorizationValue : string) {
         let tok = authorizationValue.substring(7);
         let user : LocalUser = {
-<<<<<<< HEAD
             token: tok,
             email: this.jwtHelper.decodeToken(tok).sub
-=======
-            token: tok
->>>>>>> 20ae5ed85df034746858a297f89e660e4948308c
         };
         this.storage.setLocalUser(user);
     }
