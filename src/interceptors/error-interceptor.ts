63b72f929d71969ffe7c7575095205cdc158a139
dldouglaslopes <dldouglaslopes@gmail.com>
2018-06-28 19:13:44 -0300

    Other errors with Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular/components/alert/alert-controller";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
-    constructor(public storage: StorageService){
+    constructor(public storage: StorageService, public alertCtrl: AlertController){
 
     }
 
@@ -28,11 +29,16 @@ export class ErrorInterceptor implements HttpInterceptor{
                     console.log(errorObj);
 
                     switch (errorObj.status) {
+                        case 401:
+                            this.handle401();                    
+                            break;
+                
                         case 403:
                             this.handle403();                    
                             break;
                     
                         default:
+                            this.handleDefaultError(errorObj);
                             break;
                     }
 
@@ -44,10 +50,39 @@ export class ErrorInterceptor implements HttpInterceptor{
         this.storage.setLocalUser(null);
     }
 
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: '401 error: authentication failed',
+            message: 'Email or password are wrong',
+            enableBackdropDismiss: false,  
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        alert.present();
+    }
+
+    handleDefaultError(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Error ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,  
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        alert.present();
+    }
 }
 
 export const ErrorInterceptorProvider = {
     provide : HTTP_INTERCEPTORS,
     useClass : ErrorInterceptor,
-    multi : true
+    multi : true,
 };
